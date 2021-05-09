42eb3eecc4a9bfbb38066b1695dd00cf2bf2feee
XDZhelheim <601251915@qq.com>
2021-05-09 11:03:18 +0800

    更新数据的时间戳

--- data_process.py
+++ data_process.py
@@ -4,8 +4,21 @@ import json
 import argparse
 
 
+def parse_time(t: str):
+    """Convert timestamp to seconds
+
+    Args:
+        t (str): timestamp in format HH:mm:ss
+
+    Returns:
+        seconds (int): seconds from 00:00:00
+    """
+    h_m_s = t.split(":")
+    return int(h_m_s[0]) * 60 * 60 + int(h_m_s[1]) * 60 + int(h_m_s[2])
+
+
 def add_to_dict(row: pd.Series, path_dict: dict):
-    path_dict["path"].append([row["lat"], row["lon"], row["order"]])
+    path_dict["path"].append([row["lat"], row["lon"], parse_time(row["time"])])
 
 
 if __name__ == "__main__":
@@ -25,7 +38,6 @@ if __name__ == "__main__":
     for id in id_list:
         df_id = df.loc[df["taxi_id"] == id]
         df_id = df_id.sort_values("time")
-        df_id["order"] = range(len(df_id))
 
         path_dict = {"path": []}
         df_id.apply(add_to_dict, args=(path_dict, ), axis=1)
@@ -39,4 +51,5 @@ if __name__ == "__main__":
         f.write("let taxi_tracks = ")
         json.dump(all_path, f)
 
-    print("Finished writing data to ./data/taxi_data_{}.js".format(num_of_rows))
+    print(
+        "Finished writing data to ./data/taxi_data_{}.js".format(num_of_rows))
