6d651f50146d5e695623005c56cd3bb485975625
XDZhelheim <601251915@qq.com>
2021-05-23 16:26:06 +0800

    等车时间预估

--- taxi_trace/back_end.py
+++ taxi_trace/back_end.py
@@ -18,10 +18,10 @@ def parse_time(t: str):
     h_m_s = t.split(":")
     return int(h_m_s[0]) * 60 * 60 + int(h_m_s[1]) * 60 + int(h_m_s[2])
 
-df=pd.read_csv("../sample_taxi_after_clean.csv", nrows=100000)
+df=pd.read_csv("../sample_taxi_after_clean.csv", nrows=1000000)
 df["time"]=df["time"].apply(parse_time)
 
-k=1000
+k=10000
 
 @app.route("/")
 def index():
@@ -42,12 +42,31 @@ def get_wait_time():
     df_after["distance"]=abs(df_after["lon"]-location[1])+abs(df_after["lat"]-location[0])
     df_after=df_after.sort_values("distance")
 
-    df_k=df_after.loc[df_after["distance"]<0.1][0:k]
+    df_k=df_after.loc[df_after["distance"]<0.005][0:k]
 
     if not len(df_k):
         return "No enough data"
 
-    estimate_wait_time=df_k["time"].sum()/len(df_k)-curr_time
+    time_1=df_k.loc[df["time"]-curr_time<=300, "time"]
+    if len(time_1):
+        less_than_5=time_1.sum()/len(time_1)-curr_time
+    else:
+        less_than_5=0
+
+    time_2=df_k.loc[(df["time"]-curr_time>300) & (df["time"]-curr_time<=900), "time"]
+    if len(time_2):
+        g_5_l_15=time_2.sum()/len(time_2)-curr_time
+    else:
+        g_5_l_15=0
+
+    time_3=df_k.loc[df["time"]-curr_time>900, "time"]
+    if len(time_3):
+        greater_than_15=time_3.sum()/len(time_3)-curr_time
+    else:
+        greater_than_15=0
+
+    estimate_wait_time=0.6*less_than_5+0.25*g_5_l_15+0.15*greater_than_15
+
     estimate_wait_time=round(estimate_wait_time)
     estimate_wait_time=str(datetime.timedelta(seconds=estimate_wait_time))
     app.logger.debug(estimate_wait_time)
