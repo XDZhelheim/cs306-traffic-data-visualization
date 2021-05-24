00aebd0df45104073b93cc4ec5f536d87e1e8c4f
Zhang-Muyang <73475399+Zhang-Muyang@users.noreply.github.com>
2021-05-24 11:30:46 +0800

    Update back_end.py

--- taxi_trace/back_end.py
+++ taxi_trace/back_end.py
@@ -97,10 +97,13 @@ def get_distance():
 
     data['near_distance'] = data.apply (lambda row: dist_func(location, row), axis=1)
     data_sorted = data.sort_values('near_distance')
-    data_sorted=data_sorted.loc[data_sorted["near_distance"] <= 5]
+    data_sorted=data_sorted.loc[data_sorted["near_distance"] <= 7]
     total = data_sorted['distance'].sum()
     row_count = data_sorted.shape[0]
-    return str(round(total/row_count, 2))
+    if row_count == 0:
+        return "not enough data!"
+    else:
+        return str(round(total/row_count, 2))
 
 if __name__ == "__main__":
     app.run(debug=True)
